0df11fe6022235a2ce29a8ce494920ea530b6175
nick200555 <nick.m.briggs@gmail.com>
2026-04-26 20:07:35 +0530

    Chore: Finalize repository structure to exact SBPMS specifications

---
+++ salon_management/config/docs.py
@@ -0,0 +1,9 @@
+# Documentation configuration
+
+source_path = "salon_management"
+app_name = "salon_management"
+app_title = "Salon Management"
+app_publisher = "Your Company Name"
+app_description = "Salon Management System built on Frappe"
+app_email = "contact@yourcompany.com"
+app_license = "mit"
